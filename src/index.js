18cd9f3848d511acc33b08e465846039a0916efc
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-26 22:29:26 +0500

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+  if(number/1 === 0) {
+    return 'zero'
+  }
+  let arrBeg = [' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen']
+  let arrTen = [' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety']
+  let res = []
+
+  let hundred = Math.floor(number / 100)
+  let tens = Math.floor(number % 100 / 10)
+  let ones = number % 10
+   
+  if(hundred != 0) {
+    res.push(arrBeg[hundred-1] + ' hundred')
+  } 
+
+  if(tens >= 2) {
+    res.push(arrTen[tens - 2])
+    res.push(arrBeg[ones - 1])
+  } else {
+    res.push(arrBeg[number%100 - 1])
+  }
+  return res.join('')
 }
